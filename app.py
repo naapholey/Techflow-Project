56ce1e671c0861c5ed3b339e6cdf3de6413eb70d
naapholei <annettetettey@gmail.com>
2026-08-27 19:17:06 +0000

    correcgt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! 🚀 TechFlow CI/CD Pipeline is live.", 500
+    return "Hello, World! 🚀 TechFlow CI/CD Pipeline is live.", 200
 
 @app.route("/health")
 def health():
